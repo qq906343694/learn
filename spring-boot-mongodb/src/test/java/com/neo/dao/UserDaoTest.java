bd343f7a20ba909e6ebfdeb31cb16ff257fbf173
xiongpiaohai <xiongpiaohai@163.com>
2018-05-14 14:34:18 +0800

    增加lombok使用

--- spring-boot-mongodb/src/test/java/com/neo/dao/UserDaoTest.java
+++ spring-boot-mongodb/src/test/java/com/neo/dao/UserDaoTest.java
@@ -1,11 +1,12 @@
 package com.neo.dao;
 
-import com.neo.entity.UserEntity;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.neo.entity.UserEntity;
 
 /**
  * Created by summer on 2017/5/5.
@@ -14,36 +15,36 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class UserDaoTest {
 
-    @Autowired
-    private UserDao userDao;
+	@Autowired
+	private UserDao userDao;
 
-    @Test
-    public void testSaveUser() throws Exception {
-        UserEntity user=new UserEntity();
-        user.setId(2l);
-        user.setUserName("小明");
-        user.setPassWord("fffooo123");
-        userDao.saveUser(user);
-    }
+	@Test
+	public void testSaveUser() throws Exception {
+		UserEntity user = new UserEntity();
+		user.setId(3l);
+		user.setUserName("xiaohong");
+		user.setPassWord("fffooo123");
+		userDao.saveUser(user);
+	}
 
-    @Test
-    public void findUserByUserName(){
-       UserEntity user= userDao.findUserByUserName("小明");
-       System.out.println("user is "+user);
-    }
+	@Test
+	public void findUserByUserName() {
+		UserEntity user = userDao.findUserByUserName("xiaohong");
+		System.out.println("user is " + user);
+	}
 
-    @Test
-    public void updateUser(){
-        UserEntity user=new UserEntity();
-        user.setId(2l);
-        user.setUserName("天空");
-        user.setPassWord("fffxxxx");
-        userDao.updateUser(user);
-    }
+	@Test
+	public void updateUser() {
+		UserEntity user = new UserEntity();
+		user.setId(2l);
+		user.setUserName("天空");
+		user.setPassWord("fffxxxx");
+		userDao.updateUser(user);
+	}
 
-    @Test
-    public void deleteUserById(){
-        userDao.deleteUserById(1l);
-    }
+	@Test
+	public void deleteUserById() {
+		userDao.deleteUserById(1l);
+	}
 
 }
